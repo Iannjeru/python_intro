9c36fd2c15d3f6f0b30c74f79fa98219559c01df
Ian-dev <njeruian52@gmail.com>
2025-07-30 21:01:31 +0300

    Added calculator file

---
+++ simple _cal.py
@@ -0,0 +1,15 @@
+first_Number = input("Enter first number: ")
+second_Number = input("Enter second number: ")
+#This is my addition
+sum = int(first_Number) + int(second_Number)
+#This is my subtraction
+subtraction = int(first_Number) - int(second_Number)
+#This is my multiplication
+product= int(first_Number) * int(second_Number)
+#This is my division
+division = float(first_Number) / float(second_Number)
+print(f"Sum : {sum}")
+print(f"Subtraction : {subtraction}")
+print(f"Product : {product}") 
+print(f"division: {division}")  
+
